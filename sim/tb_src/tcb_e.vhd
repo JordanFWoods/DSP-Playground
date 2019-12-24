e4e9c8ed34a01c792ede451618d040b499132bbc
JordanFWoods <jordan.f.woods@gmail.com>
2019-12-24 09:29:18 -0800

    Initial creation of tcb entity file.

---
+++ sim/tb_src/tcb_e.vhd
@@ -0,0 +1,57 @@
+------------------------------------------------
+-- Proprietary Property of:
+--
+-- Jordan Woods
+-- 312 Calle Bonita
+-- Escondido CA, 92029
+--
+------------------------------------------------
+-- Title:
+--
+-- File Name:
+--
+-- Author:
+--
+-- HDL: VHDL-93
+--
+-- Description:
+--
+------------------------------------------------
+-- Manual Revision History:
+-- 12/23/19 - JFW - Initial Check In.
+--
+------------------------------------------------
+-- Libraries:
+library std;
+use std.standard.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+-- use work.synth_pkg.all;
+
+-- library sim_work;
+-- use sim_work.testbench_pkg.all;
+
+library osvvm;
+context osvvm;
+
+------------------------------------------------
+-- entity: Testbench Control Block
+-- ADD COMMENT HERE.
+
+entity tcb is
+   generic (
+      G_SCENARIO : string := G_SCENARIO
+   );
+   port (
+      CLK       : in    std_logic;
+      RST       : in    std_logic;
+      DONE      : out   std_logic;
+      CLK_XCVR  : inout clk_xcvr;
+      BFM_XCVR  : inout bfm_xcvr
+      );
+end entity tcb;
+
